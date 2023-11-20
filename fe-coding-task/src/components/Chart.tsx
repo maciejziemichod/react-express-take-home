870feed616bbd1d6e4b2f34252336a8065f31aee
maciejziemichod <maciej.ziemichod@gmail.com>
2023-11-21 00:46:24 +0100

    feat: add chart

--- fe-coding-task/src/components/Chart.tsx
+++ fe-coding-task/src/components/Chart.tsx
@@ -1,3 +1,31 @@
-export function Chart() {
-    return <div>x</div>;
+import { CircularProgress } from "@mui/material";
+import { LineChart } from "@mui/x-charts";
+
+type ChartProps = {
+    xData: string[];
+    data: number[];
+    isLoading: boolean;
+};
+
+export function Chart({ xData, data, isLoading }: ChartProps) {
+    if (isLoading) {
+        return <CircularProgress />;
+    }
+
+    return (
+        <LineChart
+            xAxis={[
+                {
+                    data: xData.map((_, i) => i),
+                    valueFormatter: (v) => xData[v],
+                },
+            ]}
+            series={[
+                {
+                    data,
+                },
+            ]}
+            height={450}
+        />
+    );
 }
